c566de4b431e3e9b42e043a787f94722f6b39992
zuliana21 <zuli25176@gmail.com>
2022-09-28 07:28:32 +0700

    update jobsheet12

--- app/src/main/java/com/example/jobsheet12zuliana/MainActivity.kt
+++ app/src/main/java/com/example/jobsheet12zuliana/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.jobsheet12zuliana
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button1)
+        val btn2 = findViewById<Button>(R.id.button2)
+
+        btn.setOnClickListener {
+
+            val Intent = Intent(  this, MainActivity2::class.java)
+            startActivity(Intent)
+        }
+        btn2.setOnClickListener {
+
+            val Intent = Intent(  this, MainActivity3::class.java)
+            startActivity(Intent)
+        }
     }
 }
--- app/src/main/java/com/example/jobsheet12zuliana/MainActivity2.kt
+++ app/src/main/java/com/example/jobsheet12zuliana/MainActivity2.kt
@@ -1,11 +1,14 @@
 package com.example.jobsheet12zuliana
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 }
